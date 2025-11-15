1c1f366a3b789d106b84eb0500f3875750e865dd
akritijain06 <09akritijain@gmail.com>
2025-11-15 16:36:51 +0000

    Deploying to gh-pages from @ akritijain06/akritijain06.github.io@4b0e57ad9287a7c76f2f09ac84153cc4e2766397 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -23,6 +23,13 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/publications/";
           },
+        },{id: "nav-patents",
+          title: "patents",
+          description: "Filed and approved patents from my research work.",
+          section: "Navigation",
+          handler: () => {
+            window.location.href = "/patents/";
+          },
         },{id: "nav-cv",
           title: "cv",
           description: "This is a description of the page. You can modify it in &#39;_pages/cv.md&#39;. You can also change or remove the top pdf download button.",
