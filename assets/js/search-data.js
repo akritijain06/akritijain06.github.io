1b5da3ffd04e19c6a8e0ca28d987aecc72a70618
akritijain06 <09akritijain@gmail.com>
2026-01-09 04:44:20 +0000

    Deploying to gh-pages from @ akritijain06/akritijain06.github.io@5582cfa69cec41c11c12991907c5639d99f877d0 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -28,7 +28,7 @@ ninja.data = [{
           description: "",
           section: "Navigation",
           handler: () => {
-            window.location.href = "/assets/pdf/resume_final.pdf";
+            window.location.href = "/assets/pdf/Resume_updated_eacl.pdf";
           },
         },{id: "post-a-post-with-plotly-js",
         
