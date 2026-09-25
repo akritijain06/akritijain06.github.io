1865f652ab2125d187411438da1da5a2b71fac64
akritijain06 <09akritijain@gmail.com>
2026-09-25 23:48:12 +0000

    Deploying to gh-pages from @ akritijain06/akritijain06.github.io@f6ff6caec1842648de66b6e0ea52440b4743da1b 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -9,8 +9,8 @@ ninja.data = [{
     handler: () => {
       window.location.href = "/";
     },
-  },{id: "nav-selected-publications",
-          title: "selected publications",
+  },{id: "nav-publications",
+          title: "publications",
           description: "All publications in reversed chronological order.",
           section: "Navigation",
           handler: () => {
