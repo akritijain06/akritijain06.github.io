f46d18f954c0fc64af2931d90885a494dfa4a57f
akritijain06 <09akritijain@gmail.com>
2026-09-26 01:04:31 +0000

    Deploying to gh-pages from @ akritijain06/akritijain06.github.io@d53bcb97fa1bf9e7c7c5700c090e7c1837760ee4 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -398,13 +398,11 @@ ninja.data = [{
           description: "",
           section: "Books",handler: () => {
               window.location.href = "/books/the_godfather/";
-            },},{id: "news-a-long-announcement-with-details",
-          title: 'A long announcement with details',
-          description: "",
-          section: "News",handler: () => {
-              window.location.href = "/news/announcement_2/";
             },},{id: "news-a-simple-inline-announcement-with-markdown-emoji-sparkles-smile",
           title: 'A simple inline announcement with Markdown emoji! :sparkles: :smile:',
+          description: "",
+          section: "News",},{id: "news-joined-adobe-research-as-a-research-associate",
+          title: 'Joined Adobe Research as a Research Associate!',
           description: "",
           section: "News",},{id: "news-started-my-phd-in-computer-science-at-cornell-university-awarded-a-cornell-university-graduate-fellowship",
           title: 'Started my PhD in Computer Science at Cornell University. Awarded a Cornell University...',
