526f937610f20d80ea56772ba31cb11860f9af24
akritijain06 <09akritijain@gmail.com>
2026-09-26 01:55:04 +0000

    Deploying to gh-pages from @ akritijain06/akritijain06.github.io@f1107a3b21972f5a713a07349755c4f1f1476bd5 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -399,13 +399,13 @@ ninja.data = [{
           section: "Books",handler: () => {
               window.location.href = "/books/the_godfather/";
             },},{id: "news-graduate-from-iit-roorkee-awarded-the-department-gold-medal-for-highest-gpa",
-          title: 'Graduate from IIT Roorkee.     Awarded the Department Gold Medal for highest GPA.',
+          title: 'Graduate from IIT Roorkee  Awarded the Department Gold Medal for highest GPA',
           description: "",
           section: "News",},{id: "news-spent-a-fantastic-2-years-at-adobe-research-as-a-research-associate-published-papers-and-filed-multiple-patents-for-my-work",
           title: 'Spent a fantastic 2 years at Adobe Research as a Research Associate! Published...',
           description: "",
           section: "News",},{id: "news-started-my-phd-in-computer-science-at-cornell-university-awarded-cornell-university-fellowship",
-          title: 'Started my PhD in Computer Science at Cornell University. Awarded Cornell University Fellowship...',
+          title: 'Started my PhD in Computer Science at Cornell University Awarded Cornell University Fellowship...',
           description: "",
           section: "News",},{id: "projects-project-1",
           title: 'project 1',
